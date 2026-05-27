46ff92f0766561141f0a6528867f7b1112d91f82
samflynn98 <sam.flynn98@outlook.com>
2026-05-27 13:32:36 +0100

    add and modify initial setup

---
+++ src/test/java/AddTests.java
@@ -0,0 +1,33 @@
+// in MakersSearchTest.java
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddTests {
+    private static ChromeDriver driver;
+
+    @BeforeAll
+    static void launchBrowser() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void addItem() throws Exception {
+        driver.get("https://todomvc.com/examples/react/dist/#/active");
+        WebElement searchBar = driver.findElement(By.id("todo-input"));
+        searchBar.sendKeys("test");
+        searchBar.sendKeys(Keys.ENTER);
+        WebElement item = driver.findElement(By.cssSelector("[data-testid='todo-item-label']"));
+        String itemName = item.getText();
+        assertEquals(itemName, "test");
+        Thread.sleep(5000);
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        driver.quit();
+    }
+}
--- src/test/java/ModifyTests.java
+++ src/test/java/ModifyTests.java
@@ -3,14 +3,10 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MainTests {
+public class ModifyTests {
     private static ChromeDriver driver;
 
     @BeforeAll
